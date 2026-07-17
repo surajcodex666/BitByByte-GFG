0ac9c5b66dd9511d6594a8ef85c8f97aeab5c0d4
Suraj Yadav <surajyadavcodex666@gmail.com>
2026-07-17 09:14:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.java
+++ Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.java
@@ -1,41 +1,39 @@
 class Solution {
-    public double minMaxDist(int[] arr, int k) {
-        
-        if (arr.length <= 1) {
-        return 0.0;
-        }
-        // code here
-        int n = arr.length;
-        int[] howMany = new int[n - 1]; // Number of gas stations in each segment
 
-        // Place each gas station
-        for (int gasStations = 1; gasStations <= k; gasStations++) {
-            double maxSection = -1;
-            int maxInd = -1;
+    public double minMaxDist(int[] arr, int K) {
 
-            // Find the segment with the maximum distance
-            for (int i = 0; i < n - 1; i++) {
-                double diff = arr[i + 1] - arr[i];
-                double sectionLength = diff / (howMany[i] + 1.0);
+        double low = 0;
+        double high = arr[arr.length - 1] - arr[0];
 
-                if (sectionLength > maxSection) {
-                    maxSection = sectionLength;
-                    maxInd = i;
-                }
+        while (high - low > 1e-6) {
+
+            double mid = low + (high - low) / 2;
+
+            if (canPlace(arr, K, mid)) {
+                high = mid;
+            } else {
+                low = mid;
             }
-
-            // Add gas station to the selected segment
-            howMany[maxInd]++;
         }
 
-        // Find the final max section length after placing all gas stations
-        double maxAns = -1;
-        for (int i = 0; i < n - 1; i++) {
-            double diff = arr[i + 1] - arr[i];
-            double sectionLength = diff / (howMany[i] + 1.0);
-            maxAns = Math.max(maxAns, sectionLength);
+        return high;
+    }
+
+    private boolean canPlace(int[] arr, int K, double dist) {
+
+        int requiredStations = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            double gap = arr[i + 1] - arr[i];
+
+            requiredStations += (int)(gap / dist);
+
+            if (gap == ((int)(gap / dist)) * dist) {
+                requiredStations--;
+            }
         }
 
-        return maxAns;
+        return requiredStations <= K;
     }
 }
