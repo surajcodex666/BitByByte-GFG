2cb4f7cbe533fad23de60a7928bb6440677bd2da
Suraj Yadav <surajyadavcodex666@gmail.com>
2026-08-06 22:03:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Binary subarray sum/binary-subarray-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int goal) {
+        // code here
+        return atMost(nums, goal) - atMost(nums, goal-1);
+        
+    }
+
+    private int atMost(int []nums, int k){
+        
+        int n = nums.length;
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int cnt = 0; 
+
+        if(k < 0){
+            return 0;
+        }
+
+        while(r < n){
+            sum += nums[r];
+
+            while(sum > k){
+                sum -= nums[l];
+                l++;
+            }
+            cnt = cnt + ( r - l + 1);
+            r++;
+        }
+        return cnt;
+    }
+}
+   
